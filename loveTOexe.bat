6e770f9393ff7dc160b1bc4a590a21cbc67b9ff2
rostuhan <71077392+rostuhan@users.noreply.github.com>
2021-02-13 21:52:37 +0200

    Add files via upload

---
+++ loveTOexe.bat
@@ -0,0 +1,21 @@
+@echo off
+mkdir game
+copy /b love.exe+game.love game\game.exe
+copy love.dll game
+copy lua51.dll game
+copy mpg123.dll game
+copy msvcp120.dll game
+copy msvcr120.dll game
+copy OpenAL32.dll game
+copy SDL2.dll game
+echo .exe file with .dll's is done check "game" folder
+echo I
+echo I
+echo I
+echo I
+echo I
+echo I
+echo I
+echo thanks for using this .bat file
+echo       (made by rostuhan)
+pause
